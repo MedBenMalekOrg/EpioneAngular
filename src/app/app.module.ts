90d350de20b12b64a6e157819efc03f18c5433e3
Unknown <mohamed.benmalekcherif@esprit.tn>
2018-12-12 10:27:56 +0100

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import {RouteModuleRoutingModule} from './route-module/route-module-routing.module';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule,
+    RouteModuleRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
